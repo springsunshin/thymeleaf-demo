9a40434f66ef38c2ccc44875d8647e2df782699d
peng123 <1325648013@qq.com>
2020-12-02 03:58:01 +0800

    第一次

---
+++ spring-boot-thymeleaf-demo/src/main/java/com/peng/controller/PersonController.java
@@ -0,0 +1,44 @@
+package com.peng.controller;
+
+import com.peng.domain.Person;
+import com.peng.service.PersonServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+/**
+ * 业务控制类 返回视图
+ */
+@Controller
+public class PersonController {
+
+    @Autowired
+    private PersonServiceImp personServiceImp;
+
+    @RequestMapping(path = "/person/list")
+    public String list(Model model){
+        List<Person> allPerson=personServiceImp.selectAll();
+        model.addAttribute("allPerson",allPerson);
+        return "list";
+    }
+
+    @RequestMapping(path = "/person/add")
+    public String add(Person person){
+        return "add";
+    }
+
+    @RequestMapping(path = "/person/addPerson")
+    public String addPerson(Person person){
+        personServiceImp.add(person);
+        return "redirect:/person/list";
+    }
+
+    @RequestMapping(path = "/person/delete")
+    public String delete(String name){
+        personServiceImp.delete(name);
+        return "redirect:/person/list";
+    }
+}
